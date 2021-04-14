b966cb4702db2acebd7a6b846313f127dc839e17
Blzbrg <randomparanoid@gmail.com>
2021-04-13 23:42:59 -0400

    Add simple block merge logic

--- src/appendish_store/order_blocks.clj
+++ src/appendish_store/order_blocks.clj
@@ -20,3 +20,53 @@
   [block-1 block-2]
   (or (within-block block-1 (::min-key block-2))
       (within-block block-1 (::max-key block-2))))
+
+(defn append
+  [first second]
+  ;; Assumption: depends on vector
+  (apply conj first second))
+
+(defn append-block
+  [old-block new-block]
+  {::min-key (::min-key old-block)
+   ::max-key (::max-key new-block)
+   ::sorted (append (::sorted old-block) (::sorted new-block))})
+
+(defn ^:private item-less-than
+  [key item]
+  (< (item->key item) key))
+
+(defn split
+  [coll split-key]
+  (let [pred (partial item-less-than split-key)]
+    [(vec (take-while pred coll)) (vec (drop-while pred coll))]))
+
+(defn select-next
+  [sort-by coll-a coll-b]
+  (let [cmp (compare (sort-by (first coll-a)) (sort-by (first coll-b)))]
+    (cond
+      (<= cmp 0) [(first coll-a) (next coll-a) coll-b]
+      (> cmp 0) [(first coll-b) coll-a (next coll-b)])))
+
+(defn merge-sorted-by
+  [sort-by coll-a coll-b]
+  (loop [new [] ; assumption: vector
+         a coll-a
+         b coll-b]
+    (cond
+      (empty? a) (append new b)
+      (empty? b) (append new a)
+      :else (let [[n new-a new-b] (select-next sort-by a b)]
+              (recur (conj new n) new-a new-b))))) ; assumption: vector
+
+(defn merge-blocks
+  [low-block high-block]
+  (if (not (overlapping? low-block high-block))
+    (append-block low-block high-block)
+    (let [; split low to get minimal part which overlaps with high
+          [unchanged-low overlap-from-low] (split (::sorted low-block) (::min-key high-block))
+          ;; merge the minimal overlap with the high block
+          merged (merge-sorted-by item->key overlap-from-low (::sorted high-block))
+          ;; new sorted is the merged followed by the unchanged
+          new-sorted (append unchanged-low merged)]
+      (sorted->block new-sorted))))
--- test/appendish_store/order_blocks_test.clj
+++ test/appendish_store/order_blocks_test.clj
@@ -30,3 +30,29 @@
     (test/is (order-blocks/overlapping? b1 b3))
     (test/is (order-blocks/overlapping? b2 b3))
     (test/is (not (order-blocks/overlapping? b1 db)))))
+
+(test/deftest test-append-block
+  (let [b1 (order-blocks/sorted->block (keys->items [1 2 3]))
+        b2 (order-blocks/sorted->block (keys->items [4 5 6]))]
+    (test/is (= (order-blocks/append-block b1 b2)
+                (order-blocks/sorted->block (keys->items [1 2 3 4 5 6]))))))
+
+(test/deftest test-merge-sorted-by
+  (let [a (keys->items [1 2 4])
+        b (keys->items [3 5])]
+    (test/is (= (order-blocks/merge-sorted-by order-blocks/item->key a b)
+                (keys->items [1 2 3 4 5])))
+    (test/is (= (order-blocks/merge-sorted-by order-blocks/item->key a [])
+                a))
+    (test/is (= (order-blocks/merge-sorted-by order-blocks/item->key b a)
+                (keys->items [1 2 3 4 5])))))
+
+(test/deftest test-merge-blocks
+  (let [b1 (order-blocks/sorted->block (keys->items [1 2 4]))
+        b2 (order-blocks/sorted->block (keys->items [3 5 6]))
+        b12 (order-blocks/sorted->block (keys->items [1 2 3 4 5 6]))
+        b3 (order-blocks/sorted->block (keys->items [5 6 7]))]
+    (test/is (= (order-blocks/merge-blocks b1 b2) b12))
+    (test/is (= (order-blocks/merge-blocks b2 b1) b12))
+    (test/is (= (order-blocks/merge-blocks b1 b3)
+                (order-blocks/sorted->block (keys->items [1 2 4 5 6 7]))))))
