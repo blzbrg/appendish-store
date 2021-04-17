4dd1832cb68e7a61ed2619f926178afc3dfde4e3
Blzbrg <randomparanoid@gmail.com>
2021-04-17 11:03:02 -0400

    Boilerplate added
    Minimal form of the template boilerplate.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject appendish-store "0.1.0-SNAPSHOT"
+  :description "A simple library for storing a mostly-append-only ordered list of items"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
---
+++ src/appendish_store/core.clj
@@ -0,0 +1,1 @@
+(ns appendish-store.core)
